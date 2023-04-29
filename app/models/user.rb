b4c298130a78af7081018eb3e9bdc1b9abf9c9c3
Jorge Rosano <jorg.ross.13@gmail.com>
2023-04-29 13:37:01 -0600

    chore: navbar menu view

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   def self.from_omniauth(auth_hash)
- 
+    
     where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
       user.provider = auth_hash.provider
       user.uid = auth_hash.uid
