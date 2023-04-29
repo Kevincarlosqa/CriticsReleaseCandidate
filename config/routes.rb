7c6e3ce2309121944ac2584adb3acc529e829ca8
Kevin Quispe <kevincarlosqa@gmail.com>
2023-04-28 21:40:42 -0500

    Chore: Finish Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :companies  do
     resources :critics, only: :create
   end
-  resources :users
+  # resources :users
   #get 'signup', to: 'users#new'
   #post 'signup', to: 'users#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
